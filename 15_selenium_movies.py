5943dc719a760bba64cd8ca4c50f3045619e4aed
Son Yoon Seok <dbstjr5517@naver.com>
2021-01-21 17:00:54 +0900

    1/21 5:00
    headless

---
+++ 15_selenium_movies.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+url = "https://play.google.com/store/movies/top"
+browser.get(url)
+
+#스크롤 내리기
+#모니터의 해상도인 1080만큼 스크롤 내리기
+#browser.execute_script("window.scrollTo(0, 1080)")
+#browser.execute_script("window.scrollTo(0, 2080)")
+
+# 화면 가장 아래로 스크롤 내리기
+#browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+import time
+interval = 2 # 2 초에 한번씩 스크롤 내림
+
+# 현재 문서 높이를 가져와서 저장
+prev_height = browser.execute_script("return document.body.scrollHeight")
+
+while True:
+    # 스크롤 가장 아래로 내림
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    #페이지 로딩 대기
+    time.sleep(interval)
+
+    # 현재 문서 높이 가져와서 저장
+    curr_height = browser.execute_script("return document.body.scrollHeight")
+    if curr_height == prev_height:
+        break
+    prev_height = curr_height
+
+print("스크롤 완료")
+
+# bs4
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+movies = soup.find_all("div", attrs={"class" : ["Vpfmgd"]})
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    
+    # 할인 전 가격 정보
+    original_price = movie.find("span", attrs={"span" : "SUZt4c djCuy"})
+    if original_price:
+        original_price = original_price.get_text()
+    else:
+        continue
+       # print(title, " <할인되지 않은 영화 제외>")
+
+    # 할인 된 가격
+    price = movie.find("span", attrs={"span":"VfPpfd ZdBevf i5DZme"}).get_text()
+
+    link = movie.find("a", attrs={"class" : "JC71ub"})["href"]
+    
+    print(f"제목 : {title}")
+    print(f"할인 전 금액 : {original_price}")
+    print(f"할인 후 금액 : {price}")
+    print("링크 : ", "https://play.google.com" + link)
+    print("-" * 100)
+
+# browser.quit()
+
